98e953e9a6ae650048467d0c035a9fcbdbcf53cb
Alex Barr <alexbarrbusiness@gmail.com>
2024-05-26 12:27:35 +1000

    db work

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { db } from "@vercel/postgres";
 import Link from "next/link";
 
 const mockUrls = [
@@ -5,23 +6,23 @@ const mockUrls = [
   "https://utfs.io/f/7c9e12c1-a6a8-4cb3-8341-426ca9526028-usljjt.jpg",
   "https://utfs.io/f/507b94a3-a9f0-437f-9e82-a22ed7443e37-mm87lp.jpg",
   "https://utfs.io/f/2a80e6ea-1afd-4fa6-adba-747c4cc3ece9-nk26k9.png",
-  "https://utfs.io/f/6570fdcc-8fc8-497d-8f1b-d9790f8e0a24-1r1n0p.jpg"
-]
+  "https://utfs.io/f/6570fdcc-8fc8-497d-8f1b-d9790f8e0a24-1r1n0p.jpg",
+];
 
-const mockImages = mockUrls.map((url, index)=> ({
-  id: index+1,
+const mockImages = mockUrls.map((url, index) => ({
+  id: index + 1,
   url,
-}))
+}));
 
-export default function HomePage() {
+export default async function HomePage() {
+  const posts = await db.query.posts.findMany();
+  console.log(posts);
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
-
-        {[...mockImages,...mockImages ,...mockImages].map((image) => (
-          <div key={image.id} className="w-48 p-4">
+        {[...mockImages, ...mockImages, ...mockImages].map((image, index) => (
+          <div key={image.id + "-" + index} className="w-48 p-4">
             <img src={image.url} alt="img" />
-
           </div>
         ))}
       </div>
