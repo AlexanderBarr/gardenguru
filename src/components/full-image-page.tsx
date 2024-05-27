012715a87df01bc3fedb44bd0fb1baaa62e580cc
Alex Barr <luke.daniels@approach-solutions.com>
2024-05-27 13:09:01 +1000

    working on modal visuals

--- src/components/full-image-page.tsx
+++ src/components/full-image-page.tsx
@@ -1,6 +1,20 @@
+/* eslint-disable @next/next/no-img-element */
 import { getImage } from "~/server/queries";
 
 export default async function FullPageImageView(props: { id: number }) {
   const image = await getImage(props.id);
-  return <img src={image.url} alt="image" className="w-96" />;
+  return (
+    <div className="flex h-full w-full min-w-0">
+      <div className="flex flex-shrink items-center justify-center">
+        <img
+          src={image.url}
+          alt="image"
+          className="flex-shrink object-contain"
+        />
+      </div>
+      <div className="flex w-48 flex-shrink-0 flex-col border-l">
+        <div className="text-xl font-bold">{image.name}</div>
+      </div>
+    </div>
+  );
 }
