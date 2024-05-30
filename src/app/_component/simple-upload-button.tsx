d09af83588b43bcb0ff34c3bfa8f9145c2443b06
Alex Barr <luke.daniels@approach-solutions.com>
2024-05-30 14:43:54 +1000

    Feat-PostHog Event when uploading

--- src/app/_component/simple-upload-button.tsx
+++ src/app/_component/simple-upload-button.tsx
@@ -73,9 +73,11 @@ function LoadingSpinnerSVG() {
 
 export function SimpleUploadButton() {
   const router = useRouter();
+  const posthog = usePostHog();
 
   const { inputProps } = useUploadThingInputProps("imageUploader", {
     onUploadBegin() {
+      posthog.capture("upload_begin");
       toast(
         <div className="flex items-center gap-2 text-xl text-black">
           <LoadingSpinnerSVG /> <span className="text-lg">Uploading...</span>
