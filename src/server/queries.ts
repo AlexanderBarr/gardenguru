50a6245fdef0296db17f0f196ea5d16f93317437
Alex Barr <luke.daniels@approach-solutions.com>
2024-05-30 15:29:18 +1000

    Feat- Analytics on delete button

--- src/server/queries.ts
+++ src/server/queries.ts
@@ -4,6 +4,7 @@ import { auth } from "@clerk/nextjs/server";
 import { images } from "./db/schema";
 import { and, eq } from "drizzle-orm";
 import { redirect } from "next/navigation";
+import analyticsServerClient from "./analytics";
 
 export async function getMyImages() {
   const user = auth();
@@ -36,5 +37,12 @@ export async function deleteImage(id: number) {
     .delete(images)
     .where(and(eq(images.id, id), eq(images.userId, user.userId)));
 
+  analyticsServerClient.capture({
+    distinctId: user.userId,
+    event: "delete image",
+    properties: {
+      imageId: id,
+    },
+  });
   redirect("/");
 }
